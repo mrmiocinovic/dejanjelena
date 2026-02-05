085c8e23b828ff367edeb4f4a34c60afb6f64cf8
Dejan Miocinovic <mr.miocinovic@gmail.com>
2026-02-05 16:15:49 +0100

    removed console.log

--- api/send-rsvp.js
+++ api/send-rsvp.js
@@ -23,8 +23,6 @@ const transporter = nodemailer.createTransport({
 });
 
 export default async function handler(req, res) {
-  console.log("BODY:", req.body);
-
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
